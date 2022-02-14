6683aa54231b1f3aef01ada41f214b4f44bfb76f
Amin Zayeromali <amin.zayeromali@gmail.com>
2022-02-14 18:14:14 +0330

    new Update ...

--- my_site/my_app/urls.py
+++ my_site/my_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+# My app : domain.com/my_app/
 urlpatterns = [
-    path('',views.index,name='index') #/my_app --> Project urls.py
-    ]
+    path('', views.index, name='index')  # /my_app --> Project urls.py
+]
--- my_site/my_app/views.py
+++ my_site/my_app/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Index of My app")
+    return HttpResponse("Index of My app")  # template html
